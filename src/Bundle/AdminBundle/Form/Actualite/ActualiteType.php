b43509b5d6465d1884cf811ca62f66d3993c998d
qguillou <guillou.quentin.29@gmail.com>
2017-06-03 18:45:11 +0200

    Suppression des imports inutilisés

--- src/Bundle/AdminBundle/Form/Actualite/ActualiteType.php
+++ src/Bundle/AdminBundle/Form/Actualite/ActualiteType.php
@@ -7,10 +7,8 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\ResetType;
-use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 
 class ActualiteType extends AbstractType
 {
